42d146cd4001daa22f10d40469e20be54dcc8c70
ShadeeCat <assulema@gmail.com>
2018-03-13 20:32:53 +0200

    modified accesibility of homepage and added enter fields for friend and companies

--- comedy-home/script.js
+++ comedy-home/script.js
@@ -1,4 +1,28 @@
-var openFriends = document.querySelector('#friends');
-var openCompanies = document.querySelector('#companies');
+var buttonFriends = document.querySelector('#friends');
+var buttonCompanies = document.querySelector('#companies');
 
-openFriends.addEventListener('click');
+var choice_block = document.querySelector('.enter_members');
+var friendsBlock = document.querySelector('.friends');
+var companiesBlock = document.querySelector('.companies');
+
+var buttonBack1 = document.querySelector('.back1');
+var buttonBack2 = document.querySelector('.back2')
+
+buttonFriends.addEventListener('click', changeFriends);
+function changeFriends() {
+	friendsBlock.classList.remove('hidden');
+	choice_block.style.display = "none";
+}
+buttonCompanies.addEventListener('click', changeCompanies);
+function changeCompanies() {
+	companiesBlock.classList.remove('hidden');
+	choice_block.style.display = "none";
+}
+
+buttonBack1.addEventListener('click', backToChoice);
+buttonBack2.addEventListener('click', backToChoice);
+function backToChoice() {
+	friendsBlock.classList.add('hidden');
+	companiesBlock.classList.add('hidden');
+	choice_block.style.display = "flex";
+}
