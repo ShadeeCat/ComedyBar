ba069d9bd040e4f521c70e8168efff62563841cd
ShadeeCat <assulema@gmail.com>
2018-03-14 00:34:50 +0200

    modified accesibility of homepage and added enter fields for friend and companies

--- comedy-home/script.js
+++ comedy-home/script.js
@@ -4,6 +4,7 @@ var buttonCompanies = document.querySelector('#companies');
 var choice_block = document.querySelector('.enter_members');
 var friendsBlock = document.querySelector('.friends');
 var companiesBlock = document.querySelector('.companies');
+var ticketsOrder = document.querySelector('.comedy_zoa a');
 
 var buttonBack1 = document.querySelector('.back1');
 var buttonBack2 = document.querySelector('.back2')
@@ -12,11 +13,15 @@ buttonFriends.addEventListener('click', changeFriends);
 function changeFriends() {
 	friendsBlock.classList.remove('hidden');
 	choice_block.style.display = "none";
+	ticketsOrder.style.background = ("transparent");
+	ticketsOrder.style.border = ("1px solid #767676");
 }
 buttonCompanies.addEventListener('click', changeCompanies);
 function changeCompanies() {
 	companiesBlock.classList.remove('hidden');
 	choice_block.style.display = "none";
+		ticketsOrder.style.background = ("transparent");
+	ticketsOrder.style.border = ("1px solid #767676");
 }
 
 buttonBack1.addEventListener('click', backToChoice);
@@ -25,4 +30,6 @@ function backToChoice() {
 	friendsBlock.classList.add('hidden');
 	companiesBlock.classList.add('hidden');
 	choice_block.style.display = "flex";
+	ticketsOrder.style.background = ("#9e1c1e");
+	ticketsOrder.style.border = ("none");
 }
