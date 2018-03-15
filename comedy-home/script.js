f46e1120866eee9a29cf67c5d2ed774baacb359a
ShadeeCat <assulema@gmail.com>
2018-03-15 04:09:32 +0200

    added links and changed js fail in homepage

--- comedy-home/script.js
+++ comedy-home/script.js
@@ -6,30 +6,27 @@ var friendsBlock = document.querySelector('.friends');
 var companiesBlock = document.querySelector('.companies');
 var ticketsOrder = document.querySelector('.comedy_zoa a');
 
-var buttonBack1 = document.querySelector('.back1');
-var buttonBack2 = document.querySelector('.back2')
 
-buttonFriends.addEventListener('click', changeFriends);
-function changeFriends() {
+buttonFriends.addEventListener('click', function() {
 	friendsBlock.classList.remove('hidden');
 	choice_block.style.display = "none";
 	ticketsOrder.style.background = ("transparent");
 	ticketsOrder.style.border = ("1px solid #767676");
-}
-buttonCompanies.addEventListener('click', changeCompanies);
-function changeCompanies() {
+});
+buttonCompanies.addEventListener('click', function() {
 	companiesBlock.classList.remove('hidden');
 	choice_block.style.display = "none";
-		ticketsOrder.style.background = ("transparent");
+	ticketsOrder.style.background = ("transparent");
 	ticketsOrder.style.border = ("1px solid #767676");
-}
+});
 
-buttonBack1.addEventListener('click', backToChoice);
-buttonBack2.addEventListener('click', backToChoice);
-function backToChoice() {
-	friendsBlock.classList.add('hidden');
-	companiesBlock.classList.add('hidden');
-	choice_block.style.display = "flex";
-	ticketsOrder.style.background = ("#9e1c1e");
-	ticketsOrder.style.border = ("none");
+var buttonBack = document.querySelectorAll('.back')
+for (var i = 0; i < buttonBack.length; i++) {
+	buttonBack[i].addEventListener('click', function() {
+		friendsBlock.classList.add('hidden');
+		companiesBlock.classList.add('hidden');
+		choice_block.style.display = "flex";
+		ticketsOrder.style.background = ("#9e1c1e");
+		ticketsOrder.style.border = ("none");
+});
 }
